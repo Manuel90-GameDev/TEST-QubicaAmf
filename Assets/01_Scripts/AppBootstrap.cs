6ed4f84434a9d376f888c0e9871270e0457f315b
Manuel90-GameDev <55236618+Manuel90-GameDev@users.noreply.github.com>
2026-02-06 22:43:04 +0100

    Switch visualizzation
    Implemented the function to switch between DashBoard and 3D View

--- Assets/01_Scripts/AppBootstrap.cs
+++ Assets/01_Scripts/AppBootstrap.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class AppBootstrap : MonoBehaviour
 {
+    public GameObject visualizationRoot;
+    public GameObject dashboardRoot;
+
     private DashboardState dashboardState;
     private VisualizationState visualizationState;
 
     void Start()
     {
-        
+        dashboardState = new DashboardState(dashboardRoot);
+        visualizationState = new VisualizationState(visualizationRoot);
+
+        GameManager.Instance.StateMachine.ChangeState(dashboardState);
+    }
+
+    public void GoToVisualization()
+    {
+        GameManager.Instance.StateMachine.ChangeState(visualizationState);
+    }
+
+    public void GoToDashboard()
+    {
+        GameManager.Instance.StateMachine.ChangeState(dashboardState);
     }
 }
