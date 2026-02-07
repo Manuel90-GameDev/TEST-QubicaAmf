9f6a07a664a2d0c35668b5ee9048efafbe6fcfcf
Manuel90-GameDev <55236618+Manuel90-GameDev@users.noreply.github.com>
2026-02-07 11:46:01 +0100

    feature weather
    - add weather data model
    - add simulation weather service

---
+++ Assets/01_Scripts/SimulationWeatherService.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SimulationWeatherService
+{
+    public WeatherData GenerateWeather(WeatherType type)
+    {
+        return new WeatherData
+        {
+            temperature = Random.Range(-5f, 30f),
+            windSpeed = Random.Range(0f, 15f),
+            weatherType = type
+        };
+    }
+}
---
+++ Assets/01_Scripts/WeatherData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class WeatherData
+{
+    public float temperature;
+    public float windSpeed;
+    public WeatherType weatherType;
+}
