0da95608464f1947ab4e8bc9678dcd97ed85a288
Manuel90-GameDev <55236618+Manuel90-GameDev@users.noreply.github.com>
2026-02-07 16:30:25 +0100

    visualization
    - add weather listener to visualization scene
    - apply basic lighting changes based on weather
    
    BUG: does not apply the light change

---
+++ Assets/01_Scripts/Weather/VisualizationWeatherListener.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class VisualizationWeatherListener : MonoBehaviour
+{
+    [SerializeField] private Light directionalLight;
+
+    private void OnEnable()
+    {
+        Debug.Log("VisualizationWeatherListener enabled, subscribing to weather changes.");
+        WeatherSystem.Instance.Context.OnWeatherChanged += ApplyWeather;
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("VisualizationWeatherListener disabled, unsubscribing from weather changes.");
+        WeatherSystem.Instance.Context.OnWeatherChanged -= ApplyWeather;
+    }
+
+    private void ApplyWeather(WeatherData data)
+    {
+        Debug.Log($"Applying Weather: {data.weatherType}");
+
+        switch (data.weatherType)
+        {
+            case WeatherType.Clear:
+                directionalLight.color = Color.white;
+                break;
+
+            case WeatherType.Rain:
+                directionalLight.color = Color.gray;
+                break;
+
+            case WeatherType.Snow:
+                directionalLight.color = Color.cyan;
+                break;
+        }
+    }
+}
