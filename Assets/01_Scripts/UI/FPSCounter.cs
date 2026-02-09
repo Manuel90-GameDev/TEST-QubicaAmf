7060971cb298cdce5f93eb44493cf986420b5231
Manuel90-GameDev <55236618+Manuel90-GameDev@users.noreply.github.com>
2026-02-09 21:56:50 +0100

    FPS
    added FPS counter always visible on screen

---
+++ Assets/01_Scripts/UI/FPSCounter.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI fpsText;
+    [SerializeField] private float refreshRate = 1f;
+
+    private float _timer;
+    private int _frameCount;
+
+    private void Update()
+    {
+        _timer += Time.unscaledDeltaTime;
+        _frameCount++;
+
+        if (_timer >= refreshRate)
+        {
+            float fps = _frameCount / _timer;
+
+            if (fps >= 60)
+                fpsText.color = Color.green;
+            else if (fps >= 30)
+                fpsText.color = Color.yellow;
+            else
+                fpsText.color = Color.red;
+
+            fpsText.text = $"FPS: {Mathf.RoundToInt(fps)}";
+
+            _timer = 0f;
+            _frameCount = 0;
+        }
+    }
+}
